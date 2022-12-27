34cadd6c0197ae1c05fa0e9ad1ed61b83c3206db
Ali Mo'nes <AliIsHomie@outlook.com>
2022-12-27 23:44:51 +0200

    priority

--- src/app/ComponentsToShow/new-mail/new-mail.component.ts
+++ src/app/ComponentsToShow/new-mail/new-mail.component.ts
@@ -9,30 +9,30 @@ import { Email } from 'src/app/EmailType/Email';
 })
 export class NewMailComponent {
 
-  constructor(private s:ConnectorService){}
+  constructor(private s: ConnectorService) { }
 
-  @ViewChild('emailbox') emailbox:ElementRef | undefined;
-  @ViewChild('subjectbox') subjectbox:ElementRef | undefined;
-  @ViewChild('messagebox') messagebox:ElementRef | undefined;
-  
-  send(){
+  @ViewChild('emailbox') emailbox: ElementRef | undefined;
+  @ViewChild('subjectbox') subjectbox: ElementRef | undefined;
+  @ViewChild('messagebox') messagebox: ElementRef | undefined;
+
+  send() {
     let email = (<HTMLInputElement>this.emailbox?.nativeElement).value;
     let subject = (<HTMLInputElement>this.subjectbox?.nativeElement).value;
     let message = (<HTMLInputElement>this.messagebox?.nativeElement).value;
-    let e = new Email(email,subject,message,[],new Date(),this.s.ID);
+    let e = new Email(email, subject, message, [], new Date(), this.s.ID, 1);
     this.s.ID++;
     this.s.emails.push(e);
     this.s.allMails.push(e);
   }
 
-  Drafts(){
+  Drafts() {
     let email = (<HTMLInputElement>this.emailbox?.nativeElement).value;
     let subject = (<HTMLInputElement>this.subjectbox?.nativeElement).value;
     let message = (<HTMLInputElement>this.messagebox?.nativeElement).value;
-    let e = new Email(email,subject,message,[],new Date(),this.s.ID);
+    let e = new Email(email, subject, message, [], new Date(), this.s.ID, 1);
     this.s.ID++;
     this.s.Draftemails.push(e);
     this.s.allMails.push(e);
   }
-  
+
 }
