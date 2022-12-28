2eaf20c2e4c4829b53e74ce426b36f94a21c8629
Ali Mo'nes <AliIsHomie@outlook.com>
2022-12-28 10:23:01 +0200

    fixed routing hopefully

--- src/app/Router.module.ts
+++ src/app/Router.module.ts
@@ -8,29 +8,40 @@ import { TrashComponent } from './ComponentsToShow/trash/trash.component';
 import { DraftComponent } from './ComponentsToShow/Draft/draft.component';
 import { NewMailComponent } from './ComponentsToShow/new-mail/new-mail.component';
 import { SentEmailsComponent } from './ComponentsToShow/sent/sent-emails/sent-emails.component';
+import { LoginComponent } from './login/login.component';
+import { MailPageComponent } from './mail-page/mail-page.component';
 
 
 const appRoute: Routes = [
-  { path: '', component: InboxComponent },
-  { path: 'Inbox', component: InboxComponent },
-  { path: 'Sent', component: SentComponent },
-  { path: 'Trash', component: TrashComponent },
-  { path: 'draft', component: DraftComponent },
-  { path: 'new-mail', component: NewMailComponent },
-  { path: 'Sent' ,children:[
-    {path: 'sentemails/:id',component:SentEmailsComponent}
-  ]},
-  {path: 'Trash' ,children:[
-    {path: 'sentemails/:id',component:SentEmailsComponent}
-  ]},
-  {path: 'draft' ,children:[
-    {path: 'sentemails/:id',component:SentEmailsComponent}
-  ]}
+  { path: '', component: LoginComponent },
+  {
+    path: 'mail-page',
+    component: MailPageComponent,
+    children: [
+      { path: 'inbox', component: InboxComponent, outlet: 'main' },
+      { path: 'sent', component: SentComponent, outlet: 'main' },
+      { path: 'trash', component: TrashComponent, outlet: 'main' },
+      { path: 'draft', component: DraftComponent, outlet: 'main' },
+      { path: 'new-mail', component: NewMailComponent, outlet: 'main' },
+
+      { path: 'sentemails/:id', component: SentEmailsComponent, outlet: 'main' },
+      // {
+      //   path: 'trash', children: [
+      //     { path: 'sentemails/:id', component: SentEmailsComponent }
+      //   ]
+      // },
+      // {
+      //   path: 'draft', children: [
+      //     { path: 'sentemails/:id', component: SentEmailsComponent }
+      //   ]
+      // }
+    ]
+  },
 ]
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoute)
+    RouterModule.forRoot(appRoute),
   ],
   exports: [
     RouterModule
