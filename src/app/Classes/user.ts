66ecde5e541a9684ebc1628026ea0274220c48de
Marwan <maroelsays2013@gmail.com>
2022-12-31 23:27:12 +0200

    t3del

--- src/app/Classes/user.ts
+++ src/app/Classes/user.ts
@@ -54,6 +54,8 @@ export class User {
 
     deleteContact(contact: Contact) {
         this.contacts.splice(this.contacts.indexOf(contact), 1);
+    }
+    
     deleteAll(email: Email[], root: string) {
         if (root != 'trash')
             this.trash.push(...email);
