6735eb25dfc7d3d396712e4cec7dd7f6f8b8e238
Ali Mo'nes <AliIsHomie@outlook.com>
2023-01-01 14:15:12 +0200

    visual

--- src/app/services/connector.service.ts
+++ src/app/services/connector.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class ConnectorService {
   private _ID: number = 0;
   private _hideMenu: boolean = false;
-  private _allMails: Email[] = [];
+  // private _allMails: Email[] = [];
   // private _users: User[] = [new User("Ali", "Mones", new Date(2003, 2, 3), Gender.male, "ali@m.com", "pwp"), 
   //                           new User("Marwan", "Mostafa", new Date(2002, 1, 28), Gender.male, 'marwan@m.com', "wow")];
   private _activeUserID: number = 0;
