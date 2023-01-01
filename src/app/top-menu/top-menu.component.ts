d86f6268fd1524da0180fe70a0ee096dfb13a508
Marwan <maroelsays2013@gmail.com>
2023-01-01 20:44:08 +0200

    search

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,19 +1,46 @@
 import { ConnectorService } from './../services/connector.service';
-import { Component } from '@angular/core';
-import { faBars, faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { faBars, faMagnifyingGlass, faSliders } from '@fortawesome/free-solid-svg-icons';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-top-menu',
   templateUrl: './top-menu.component.html',
   styleUrls: ['./top-menu.component.css']
 })
-export class TopMenuComponent {
+export class TopMenuComponent implements OnInit {
 
   constructor(private s: ConnectorService) { }
   faBars = faBars;
   faMagnifyingGlass = faMagnifyingGlass;
-  faXmark = faXmark;
+  faSliders = faSliders;
+  hide:boolean = true;
+  reactiveForm!:FormGroup;
 
+  ngOnInit(): void {
+   this.reactiveForm = new FormGroup({
+    sender:new FormControl(null,Validators.email),
+    Receiver:new FormControl(null,Validators.email),
+    Importance:new FormControl(null),
+    Subject:new FormControl(null),
+    Body:new FormControl(null),
+    Attachments:new FormControl(null),
+    Folder:new FormControl(null),
+   })
+  }
+
+  onSubmit(){
+    let SearchArray:string[] = [];
+    SearchArray.push(this.reactiveForm.get('sender')?.value);
+    SearchArray.push(this.reactiveForm.get('Receiver')?.value);
+    SearchArray.push(this.reactiveForm.get('Importance')?.value);
+    SearchArray.push(this.reactiveForm.get('Subject')?.value);
+    SearchArray.push(this.reactiveForm.get('Body')?.value);
+    SearchArray.push(this.reactiveForm.get('Attachments')?.value);
+    SearchArray.push(this.reactiveForm.get('Folder')?.value);
+    this.hide = false;
+    console.log(SearchArray);
+  }
 
   Do() {
     this.s.toggleMenu();
